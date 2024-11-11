7a8f89f82f60207c83be3ee20e66f02f831b4866
amanliag <liagre.amandinee@gmail.com>
2024-11-11 17:31:51 +0100

    Gestion des librairies

---
+++ dependencies.R
@@ -0,0 +1,17 @@
+# LIST OF REQUIRED PACKAGES -----------------------------------------------
+
+required_packages <- c(
+  "checkpoint"
+)
+
+# install missing packages
+
+new.packages <- required_packages[!(required_packages %in% installed.packages()[,"Package"])]
+
+if (length(new.packages)) {
+  install.packages(new.packages)
+}
+
+rm(new.packages)
+
+library(tidyr)
