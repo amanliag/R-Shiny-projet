c7e41d033b9a6a35b7c0704823d19567abe653da
amanliag <liagre.amandinee@gmail.com>
2024-11-11 17:46:31 +0100

    Début du projet

--- Impact_sobriete/app.R
+++ Impact_sobriete/app.R
@@ -1,6 +1,17 @@
 # INSTALL DEPENDENCIES ----------------------------------------------------
 
-source('dependencies.R')
+source('../dependencies.R')
 # load all packages
 lapply(required_packages, require, character.only = TRUE)
+
+ui <- fluidPage(
+  
+)
+
+server <- function(input, output) {
+  
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
 
