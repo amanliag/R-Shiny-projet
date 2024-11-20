d7ac85c2215f7404d96ece813074d2b625ccecf4
amanliag <liagre.amandinee@gmail.com>
2024-11-20 14:47:45 +0100

    Modif dependances

--- Impact_sobriete/app.R
+++ Impact_sobriete/app.R
@@ -1,12 +1,42 @@
-# INSTALL DEPENDENCIES ----------------------------------------------------
+# INSTALLATION DEPENDANCES ----------------------------------------------------
 
 source('../dependencies.R')
-# load all packages
+source('fonctions.R')
+
+# charger les paquets
 lapply(required_packages, require, character.only = TRUE)
 
-ui <- fluidPage(
+
+# ui ----------------------------------------------------
+
+ui <- fluidPage (
+######## DASHBOARD ########
+  dashboardPage(
+  dashboardHeader(title = "Coût - garde partagé"),
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Simulateur", tabName = "simulateur", icon = icon("chart-pie")),
+      menuItem("Etudes de cas", tabName = "etude_cas", icon = icon("user"))
+      )
+  ),
   
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "simulateur",
+              h2("Contenu du simulateur")
+      ),
+      tabItem(tabName = "etude_cas",
+              h2("Contenu des études de cas")
+      )
+    )
+  )
 )
+
+
+
+
+)
+
 
 server <- function(input, output) {
   
