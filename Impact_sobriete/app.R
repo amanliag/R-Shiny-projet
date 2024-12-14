939edcee6506d539eaac27bbdb2612a87736b05d
amanliag <liagre.amandinee@gmail.com>
2024-12-14 20:02:45 +0100

    Avancée affichage salaire brut et net, mensualisée

--- Impact_sobriete/app.R
+++ Impact_sobriete/app.R
@@ -20,10 +20,10 @@ ui <- fluidPage(
     dashboardBody(
       tabItems(
         ######## ONGLET INFORMATIONS POUR SIMULATEUR ########
-        
         tabItem(tabName = "simulateur",
                 h2("Informations nécessaires à la simulation"),
                 
+                # Informations pour les deux familles
                 fluidRow(
                   column(6,
                          wellPanel(
@@ -43,38 +43,15 @@ ui <- fluidPage(
                                tagList(
                                  h5(jour),
                                  fluidRow(
-                                   column(6, timeInput(paste0("debut_matin_", jour), "Début matin :", value = strptime("08:00", format = "%H:%M"))),
-                                   column(6, timeInput(paste0("fin_matin_", jour), "Fin matin :", value = strptime("12:00", format = "%H:%M")))
+                                   column(6, timeInput(paste0("debut_matin_", jour, "_f1"), "Début matin :", value = strptime("08:00", format = "%H:%M"))),
+                                   column(6, timeInput(paste0("fin_matin_", jour, "_f1"), "Fin matin :", value = strptime("12:00", format = "%H:%M")))
                                  ),
                                  fluidRow(
-                                   column(6, timeInput(paste0("debut_aprem_", jour), "Début après-midi :", value = strptime("14:00", format = "%H:%M"))),
-                                   column(6, timeInput(paste0("fin_aprem_", jour), "Fin après-midi :", value = strptime("18:00", format = "%H:%M")))
-                                 ),
-                                 checkboxGroupInput(paste0("repas_", jour), "Repas pris en compte :", 
-                                                    choices = c("Petit-déjeuner", "Midi", "Goûter", "Soir"))
+                                   column(6, timeInput(paste0("debut_aprem_", jour, "_f1"), "Début après-midi :", value = strptime("14:00", format = "%H:%M"))),
+                                   column(6, timeInput(paste0("fin_aprem_", jour, "_f1"), "Fin après-midi :", value = strptime("18:00", format = "%H:%M")))
+                                 )
                                )
                              })
-                           ),
-                           
-                           # Dates des jours non travaillés et vacances
-                           wellPanel(
-                             style = "background-color: #fff; border: 1px solid #333;",
-                             h4("Dates non travaillées :"),
-                             airDatepickerInput("jours_non_travailles_f1", 
-                                                "Jours non travaillés :", 
-                                                multiple = TRUE, value = Sys.Date()), 
-                             
-                             h4("Périodes de vacances :"),
-                             actionButton("ajout_vacances", "Ajouter une période de vacances"),
-                             uiOutput("vacances_f1") 
-                           ),
-                           #Prime de panier/remboursement du transport
-                           wellPanel(
-                             style = "background-color: #fff; border: 1px solid #333;",
-                             h4("Prime de panier :"),
-                             numericInput("prime_panier_f1", "Prime de panier (€) :", value = 7.30, min = 7.30),
-                             h4("Remboursement du titre de transport :"),
-                             numericInput("remb_transport_f1", "Remboursement (€) :", value = 0),
                            )
                          )
                   ),
@@ -102,35 +79,18 @@ ui <- fluidPage(
                                  fluidRow(
                                    column(6, timeInput(paste0("debut_aprem_", jour, "_f2"), "Début après-midi :", value = strptime("14:00", format = "%H:%M"))),
                                    column(6, timeInput(paste0("fin_aprem_", jour, "_f2"), "Fin après-midi :", value = strptime("18:00", format = "%H:%M")))
-                                 ),
-                                 checkboxGroupInput(paste0("repas_", jour, "_f2"), "Repas pris en compte :", 
-                                                    choices = c("Petit-déjeuner", "Midi", "Goûter", "Soir"))
+                                 )
                                )
                              })
-                           ),
-                           
-                           # Dates des jours non travaillés et vacances
-                           wellPanel(
-                             style = "background-color: #fff; border: 1px solid #333;",
-                             h4("Dates non travaillées :"),
-                             airDatepickerInput("jours_non_travailles_f2", 
-                                                "Jours non travaillés :", 
-                                                multiple = TRUE, 
-                                                value = Sys.Date()), 
-                             
-                             h4("Périodes de vacances :"),
-                             actionButton("ajout_vacances_f2", "Ajouter une période de vacances"),
-                             uiOutput("vacances_f2") 
-                           ),
-                           #Prime de panier/remboursement du transport
-                           wellPanel(
-                             style = "background-color: #fff; border: 1px solid #333;",
-                             h4("Prime de panier :"),
-                             numericInput("prime_panier_f2", "Prime de panier (€) :", value = 7.30),
-                             h4("Remboursement du titre de transport :"),
-                             numericInput("remb_transport_f2", "Remboursement (€) :", value = 0),
                            )
                          )
+                  )
+                ),
+                
+                fluidRow(
+                  column(12,
+                         actionButton("calcul_global", "Calculer pour les deux familles"),
+                         verbatimTextOutput("resultats_combines") 
                   )
                 )
         ),
@@ -148,53 +108,76 @@ ui <- fluidPage(
 ### SERVER
 server <- function(input, output, session) {
   
-#### Période vacances
-  compteurs_vacances_f1 <- reactiveVal(0)
-  observeEvent(input$ajout_vacances, {
-    count <- compteurs_vacances_f1() + 1
-    vacations_counter_f1(count)
-    insertUI(
-      selector = "#ajout_vacances",
+  #### Calcul des heures totales travaillées par semaine pour une famille donnée ####
+  calcul_heures_semaine <- function(input_suffixe) {
+    jours <- c("Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche")
+    total_minutes <- 0
+    
+    for (jour in jours) {
+      matin_debut <- input[[paste0("debut_matin_", jour, input_suffixe)]]
+      matin_fin <- input[[paste0("fin_matin_", jour, input_suffixe)]]
+      aprem_debut <- input[[paste0("debut_aprem_", jour, input_suffixe)]]
+      aprem_fin <- input[[paste0("fin_aprem_", jour, input_suffixe)]]
       
-      where = "afterEnd",
-      ui = dateRangeInput(paste0("vacation_f1_", count),
-                          label = paste("Période de vacances", count),
-                          start = Sys.Date(), end = Sys.Date() + 7)
-    )
-  })
-  
-  compteur_vacances_f2 <- reactiveVal(0)
-  observeEvent(input$ajout_vacances_f2, {
-    count <- compteur_vacances_f2() + 1
-    vacations_counter_f2(count)
-    insertUI(selector = "#ajout_vacances_f2",
-      where = "afterEnd",
-      ui = dateRangeInput(paste0("vacation_f2_", count),
-                          label = paste("Période de vacances", count),
-                          start = Sys.Date(), end = Sys.Date() + 7)
-    )
-  })
-  
-#### ----- Vérification heures ----- ###
-  
-  observe({ 
-  jours <- c("Lundi","Mardi","Mercredi","Jeudi","Vendredi","Samedi")
-  heures <- list()
-  for (jour in jours){
-      heures[[jour]]<-list(input[[paste0("debut_matin_", jour)]],
-                           input[[paste0("fin_matin_", jour)]],
-                           input[[paste0("debut_aprem_", jour)]],
-                           input[[paste0("fin_aprem_", jour)]])
-
+      matin_debut <- if (!is.null(matin_debut) && matin_debut != "") as.POSIXct(matin_debut, format = "%H:%M") else NULL
+      matin_fin <- if (!is.null(matin_fin) && matin_fin != "") as.POSIXct(matin_fin, format = "%H:%M") else NULL
+      aprem_debut <- if (!is.null(aprem_debut) && aprem_debut != "") as.POSIXct(aprem_debut, format = "%H:%M") else NULL
+      aprem_fin <- if (!is.null(aprem_fin) && aprem_fin != "") as.POSIXct(aprem_fin, format = "%H:%M") else NULL
+      
+      if (!is.null(matin_debut) && !is.null(matin_fin)) {
+        total_minutes <- total_minutes + as.numeric(difftime(matin_fin, matin_debut, units = "mins"))
+      }
+      
+      if (!is.null(aprem_debut) && !is.null(aprem_fin)) {
+        total_minutes <- total_minutes + as.numeric(difftime(aprem_fin, aprem_debut, units = "mins"))
+      }
+    }
+    
+    return(total_minutes / 60) # Convertir les minutes en heures
   }
   
-  res <- calcul_total_heure(heures)
-  if (res >= 2400 & res <= 2880){
-    showNotification(paste("ATTENTION :", conversion_entier_to_heure(res-2400), "seront payés 25% de plus"), type = "warning",duration = 5)
-  }else if (res >2880){
-    showNotification(paste("Vous dépassez le cadre légal (48h) de :", conversion_entier_to_heure(res-2880), "h, veuillez changer votre saisie"), type = "warning",duration = 5)
-  }
+  #### Observer le bouton pour calculer les résultats ####
+  observeEvent(input$calcul_global, {
+    heures_f1 <- calcul_heures_semaine("_f1") #Préfixe Famille 1
+    salaire_brut_f1 <- input$salaire_brut_f1
+    salaire_annuel_f1 <- salaire_brut_f1 * heures_f1 * 52
+    
+    heures_f2 <- calcul_heures_semaine("_f2") # Préfixe pour Famille 2
+    salaire_brut_f2 <- input$salaire_brut_f2
+    salaire_annuel_f2 <- salaire_brut_f2 * heures_f2 * 52
+    
+    heures_totales <- heures_f1 + heures_f2
+    salaire_annuel_total <- salaire_annuel_f1 + salaire_annuel_f2
+    
+    output$resultats_combines <- renderText({
+      paste(
+        "Résultats pour la Famille 1 :\n",
+        "- Salaire brut horaire : ", salaire_brut_f1, "€\n",
+        "- Salaire net horaire :", calcul_net(salaire_brut_f1), "€\n",
+        "- Heures par semaine : ", round(heures_f1, 2), "h\n",
+        "- Salaire annualisé : ", round(salaire_annuel_f1, 2), "€\n\n",
+        
+        "Résultats pour la Famille 2 :\n",
+        "- Salaire brut horaire : ", salaire_brut_f2, "€\n",
+        "- Salaire net horaire :", calcul_net(salaire_brut_f2), "€\n",
+        "- Heures par semaine : ", round(heures_f2, 2), "h\n",
+        "- Salaire annualisé : ", round(salaire_annuel_f2, 2), "€\n\n",
+        
+        "Résultats combinés :\n",
+        "- Heures totales par semaine : ", round(heures_totales, 2), "h\n", #A CORRIGER SELON LES PLAGES HORAIRES DES FAMILLES
+        "- Salaire annualisé total brut pour la famille 1: ", calcul_salaire_mensualise_partage(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[1],"€\n",
+        "- Salaire annualisé total brut pour la famille 2: ", calcul_salaire_mensualise_partage(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[2],"€\n",
+        "- Salaire annualisé total brut pour les deux familles: ", calcul_salaire_mensualise_partage(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[3],"€\n\n",
+        
+        "- Salaire annualisé total net pour la famille 1: ", repartition_salaire_net_mensualise(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[1],"€\n",
+        "- Salaire annualisé total net pour la famille 2: ", repartition_salaire_net_mensualise(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[2],"€\n",
+        "- Salaire annualisé total net pour les deux familles: ", repartition_salaire_net_mensualise(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[3],"€\n",
+        "- Part en pourcentage de la famille 1: ", repartition_salaire_net_mensualise(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[4],"%\n",
+        "- Part en pourcentage de la famille 2: ", repartition_salaire_net_mensualise(round(heures_f1, 2), round(heures_f2, 2), salaire_brut_f1, salaire_brut_f2, 52)[5],"%\n\n"
+      )
+    })
   })
 }
+
 # Lancer l'application
 shinyApp(ui = ui, server = server)
