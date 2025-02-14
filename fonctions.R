6e5ed109cba391a455141056446b9a1f8acc7ebc
amanliag <liagre.amandinee@gmail.com>
2025-02-14 19:06:27 +0100

    ajout des heures complémentaires et supplémentaires

--- fonctions.R
+++ fonctions.R
@@ -4,38 +4,66 @@ calcul_net <- function(salaire_brut) {
   return (ceiling(salaire_brut * (1 - 0.23)))
 }
 
-# --- Salaire brut annualisé 
-calcul_salaire_mensualise_partage <- function(heures_par_semaine_f1, heures_par_semaine_f2, tarif_horaire_f1, tarif_horaire_f2, semaines_travaillees = 52) {
+# --- Salaire brut annualisé avec heures communes et heures complémentaires/supplémentaires
+calcul_salaire_mensualise_partage <- function(heures_communes, heures_par_semaine_f1, heures_par_semaine_f2, tarif_horaire, semaines_travaillees = 52) {
+  heures_comp_communes <- 0
+  heures_sup_communes <- 0
   heures_comp_f1 <- 0
   heures_sup_f1 <- 0
   heures_comp_f2 <- 0
   heures_sup_f2 <- 0
   
-  if (heures_par_semaine_f1 > 32){
-    heures_comp_f1 <- heures_par_semaine_f1-32
-    if (heures_par_semaine_f1 > 45){
-      heures_sup_f1 <- heures_par_semaine_f1-45
-    }
-  }
-  if (heures_par_semaine_f2 > 32){
-    heures_comp_f2 <- heures_par_semaine_f2-32
-    if (heures_par_semaine_f2 > 45){
-      heures_sup_f2 <- heures_par_semaine_f2-45
-    }
-  }
-  heures_par_semaine_f1 <- heures_par_semaine_f1 - heures_sup_f1 - heures_comp_f1
-  heures_par_semaine_f2 <- heures_par_semaine_f2 - heures_sup_f2 - heures_comp_f2
+  total_heures_communes <- heures_communes
+  total_heures_f1 <- heures_communes + heures_par_semaine_f1
+  total_heures_f2 <- heures_communes + heures_par_semaine_f2
   
-    
-  salaire_f1 <- (semaines_travaillees * (heures_par_semaine_f1 * tarif_horaire_f1 + heures_comp_f1 * (tarif_horaire_f1 + 4.10) + heures_sup_f1 * (tarif_horaire_f1 + 4.48))) / 12
-  salaire_f2 <- (semaines_travaillees * (heures_par_semaine_f2 * tarif_horaire_f2 + heures_comp_f2 * (tarif_horaire_f2 + 4.10) + heures_sup_f2 * (tarif_horaire_f2 + 4.48))) / 12
+  if (total_heures_communes > 32) {
+    heures_comp_communes <- total_heures_communes - 32
+    if (total_heures_communes > 45) {
+      heures_sup_communes <- total_heures_communes - 45
+      heures_comp_communes <- 45 - 32
+    }
+  }
+  
+  if (total_heures_f1 > 32) {
+    heures_comp_f1 <- total_heures_f1 - 32
+    if (total_heures_f1 > 45) {
+      heures_sup_f1 <- total_heures_f1 - 45
+      heures_comp_f1 <- 45 - 32
+    }
+  }
+  
+  if (total_heures_f2 > 32) {
+    heures_comp_f2 <- total_heures_f2 - 32
+    if (total_heures_f2 > 45) {
+      heures_sup_f2 <- total_heures_f2 - 45
+      heures_comp_f2 <- 45 - 32
+    }
+  }
+  
+  heures_base_communes <- total_heures_communes - heures_sup_communes - heures_comp_communes
+  heures_base_f1 <- total_heures_f1 - heures_sup_f1 - heures_comp_f1 - heures_base_communes
+  heures_base_f2 <- total_heures_f2 - heures_sup_f2 - heures_comp_f2 - heures_base_communes
+  
+  salaire_communes <- (semaines_travaillees * (heures_base_communes * tarif_horaire + heures_comp_communes * (tarif_horaire + 4.10) + heures_sup_communes * (tarif_horaire + 4.48))) / 12
+  salaire_part_f1 <- salaire_communes / 2
+  salaire_part_f2 <- salaire_communes / 2
+  
+  salaire_f1 <- (semaines_travaillees * (heures_base_f1 * tarif_horaire + heures_comp_f1 * (tarif_horaire + 4.10) + heures_sup_f1 * (tarif_horaire + 4.48))) / 12 + salaire_part_f1
+  salaire_f2 <- (semaines_travaillees * (heures_base_f2 * tarif_horaire + heures_comp_f2 * (tarif_horaire + 4.10) + heures_sup_f2 * (tarif_horaire + 4.48))) / 12 + salaire_part_f2
+  
   salaire_total <- salaire_f1 + salaire_f2
-    
-  salaire_f1 <- round(salaire_f1, 2)
-  salaire_f2 <- round(salaire_f2, 2)
+  
+  salaire_communes <- round(salaire_communes, 2)
+  salaire_part_f1 <- round(salaire_part_f1, 2)
+  salaire_part_f2 <- round(salaire_part_f2, 2)
+  salaire_f1 <- round(salaire_f1, 2) + salaire_part_f1
+  salaire_f2 <- round(salaire_f2, 2) + salaire_part_f2
   salaire_total <- round(salaire_total, 2)
   
   return(list(
+    salaire_part_f1 = salaire_part_f1,
+    salaire_part_f2 = salaire_part_f2,
     salaire_f1 = salaire_f1,
     salaire_f2 = salaire_f2,
     salaire_total = salaire_total
@@ -43,19 +71,28 @@ calcul_salaire_mensualise_partage <- function(heures_par_semaine_f1, heures_par_
 }
 
 
-# --- Salaire net annualisé 
-repartition_salaire_net_mensualise <- function(heures_f1, heures_f2, tarif_horaire_brut_f1, tarif_horaire_brut_f2, semaines_travaillees = 52) {
-  tarif_net_f1 <- tarif_horaire_brut_f1 * (1 - 0.23)
-  tarif_net_f2 <- tarif_horaire_brut_f2 * (1 - 0.23)
+
+# --- Salaire net annualisé avec les heures communes
+repartition_salaire_net_mensualise <- function(heures_communes, heures_f1, heures_f2, tarif_horaire, semaines_travaillees = 52) {
+  tarif_net_communes <- tarif_horaire * (1 - 0.23)
+  tarif_net_f1 <- tarif_horaire * (1 - 0.23)
+  tarif_net_f2 <- tarif_horaire * (1 - 0.23)
   
-  contribution_net_f1 <- (heures_f1 * tarif_net_f1 * semaines_travaillees)/12
-  contribution_net_f2 <- (heures_f2 * tarif_net_f2 * semaines_travaillees)/12
+  contribution_net_communes <- (heures_communes * tarif_net_communes * semaines_travaillees) / 12
+  part_communes_f1 <- contribution_net_communes / 2
+  part_communes_f2 <- contribution_net_communes / 2
+  
+  contribution_net_f1 <- ((heures_f1 * tarif_net_f1 * semaines_travaillees) / 12) + part_communes_f1
+  contribution_net_f2 <- ((heures_f2 * tarif_net_f2 * semaines_travaillees) / 12) + part_communes_f2
   
   total_contribution_net <- contribution_net_f1 + contribution_net_f2
   
   part_f1 <- contribution_net_f1 / total_contribution_net
   part_f2 <- contribution_net_f2 / total_contribution_net
   
+  contribution_net_communes <- round(contribution_net_communes, 2)
+  part_communes_f1 <- round(part_communes_f1, 2)
+  part_communes_f2 <- round(part_communes_f2, 2)
   contribution_net_f1 <- round(contribution_net_f1, 2)
   contribution_net_f2 <- round(contribution_net_f2, 2)
   total_contribution_net <- round(total_contribution_net, 2)
@@ -63,6 +100,9 @@ repartition_salaire_net_mensualise <- function(heures_f1, heures_f2, tarif_horai
   part_f2 <- round(part_f2, 2)
   
   return(list(
+    contribution_net_communes = contribution_net_communes,
+    part_communes_f1 = part_communes_f1,
+    part_communes_f2 = part_communes_f2,
     contribution_net_f1 = contribution_net_f1,
     contribution_net_f2 = contribution_net_f2,
     total_contribution_net = total_contribution_net,
@@ -70,6 +110,7 @@ repartition_salaire_net_mensualise <- function(heures_f1, heures_f2, tarif_horai
     part_f2 = part_f2
   ))
 }
+
 
 
 # --- Calcul part employeur
